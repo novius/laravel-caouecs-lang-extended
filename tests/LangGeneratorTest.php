7aea15609a783b01741ecd874ab634c4c96af357
Tony BOTALLA <botalla@novius.fr>
2017-07-10 08:50:50 +0200

    Test with Orchestra

--- tests/LangGeneratorTest.php
+++ tests/LangGeneratorTest.php
@@ -3,11 +3,20 @@
 namespace Novius\CaouecsLangExtended\Tests;
 
 use Mockery as m;
+use Novius\Caouecs\Lang\LangGeneratorServiceProvider;
 use Orchestra\Testbench\TestCase;
 use Symfony\Component\Console\Exception\RuntimeException;
 
 class LangGeneratorTest extends TestCase
 {
+    public function createApplication()
+    {
+        $app = parent::createApplication();
+        $app->register(LangGeneratorServiceProvider::class);
+
+        return $app;
+    }
+
     public function testCallWithoutArgument()
     {
         try {
@@ -28,11 +37,4 @@ class LangGeneratorTest extends TestCase
         $this->artisan('lang:install', ['local' => 'xyz', '--force' => false]);
     }
 
-    public function testCallWithGoodLocalAndForce()
-    {
-        $command = m::mock('\Novius\Caouecs\Lang\Console\LanguageGeneratorCommand[info]', [[]]);
-        $command->shouldReceive('info')->once()->with(matchesPattern('/successfully installed/'));
-        $this->app['Illuminate\Contracts\Console\Kernel']->registerCommand($command);
-        $this->artisan('lang:install', ['local' => 'fr', '--force' => true]);
-    }
 }
